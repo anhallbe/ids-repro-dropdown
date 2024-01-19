36b378f405c946740af78993a60784157d793415
Andreas Hallberg <andreas.hallberg@infor.com>
2024-01-19 20:07:53 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { SohoComponentsModule } from 'ids-enterprise-ng';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [SohoComponentsModule, FormsModule],
+  template: `
+    <label soho-label for="dropdown">Color</label>
+    <select id="dropdown" soho-dropdown [(ngModel)]="color">
+      @for (color of colors; track color.id) {
+      <option [ngValue]="color.value" [attr.data-icon]="toIcon(color)" [attr.data-hex]="color.value">
+        {{color.name}}
+      </option>
+      }
+    </select>
+  `,
 })
 export class AppComponent {
-  title = 'repro-ids-dropdown';
+  colors = Object.values(Soho.theme.personalizationColors());
+  color = this.colors[0].value;
+
+  toIcon(color: SohoPersonalizationColor): string {
+    return JSON.stringify({
+      icon: "swatch",
+      class: `${color.backgroundColorClass} swatch}`,
+    })
+  }
 }
